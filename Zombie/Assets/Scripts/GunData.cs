26b76b2108d04cc98c35094d821ae2f06733c6aa
jinsub <jinsub1113@naver.com>
2023-08-01 16:00:33 +0900

    15장 좀비 서바이버_총과 슈터

---
+++ Zombie/Assets/Scripts/GunData.cs
@@ -0,0 +1,18 @@
+
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable/GunData", fileName = "Gun Data")]
+public class GunData : ScriptableObject
+{
+    public AudioClip shotClip; // 발사 소리
+    public AudioClip reloadClip; // 재장전 소리
+
+    public float damage = 25; // 공격력
+
+    public int startAmmoRemain = 100; // 처음 주어지는 전체 탄알
+    public int magCapacity = 25; // 탄창 용량
+
+    public float timeBetFire = 0.12f; // 탄알 발사 간격
+    public float reloadTime = 1.8f; // 재장전 소요 시간
+    
+}
